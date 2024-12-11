34092f1fcd9e91139f1eedfe9fc68eae3c94613a
Akram Moumen El Idrissi <akram.mo.gb@gmail.com>
2024-12-11 21:21:00 +0100

    Update PaymentController.java

--- src/main/java/org/microservice/paymentservice/PaymentController.java
+++ src/main/java/org/microservice/paymentservice/PaymentController.java
@@ -17,12 +17,20 @@ public class PaymentController {
     @PostMapping("")
     public ResponseEntity<?> checkToken(@RequestHeader("Authorization") String token) {
         try {
+            token = extractToken(authorizationHeader);
             System.out.println(token);
             ResponseEntity<?> response = authServiceClient.validateToken(token);
             return ResponseEntity.ok(response.getBody());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token validation failed: " + e.getMessage());
         }
+    }
+
+    private String extractToken(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7); // Remove "Bearer " prefix
+        }
+        throw new IllegalArgumentException("Invalid Authorization header");
     }
 }
 
